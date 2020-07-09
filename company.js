7a4d3fc6ec25431fb1beaa9548b603e45d9163ec
Gregory Monroe II <gregthemusician@gmail.com>
2020-07-09 01:11:51 -0400

    added initial questionnaire

--- company.js
+++ company.js
@@ -11,3 +11,58 @@ var connection = mysql.createConnection({
   password: "",
   database: "company_tracker"
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    begin();
+  });
+
+
+function begin() {
+  inquirer
+    .prompt({
+          type: "choices"
+          name: "options"
+          message: "What would you like to do?"
+          choices: [
+            "View All Employees",
+            "Vice All Employees By Department",
+            "View All Employees by Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager"
+          ]
+    }).then(function(answer) {
+            switch (answer.action) {
+                case "View All Employees":
+                allEmployees();
+                break;
+        
+                case "Vice All Employees By Department":
+                byDepartment();
+                break;
+        
+                case "View All Employees by Manager":
+                byManager();
+                break;
+        
+                case "Add Employee":
+                addEmployee();
+                break;
+        
+                case "Remove Employee":
+                removeEmployee();
+                break;
+
+                case "Update Employee Role":
+                updateRole();
+                break;
+        
+                case "Update Employee Manager":
+                updateManager();
+                break;
+            }
+        }
+  ])
+}
