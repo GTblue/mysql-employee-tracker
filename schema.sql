7886cdd8f561df2b9cf3fd889e27c21bde55c33d
Gregory Monroe II <gregthemusician@gmail.com>
2020-07-09 00:50:26 -0400

    ran npm init, generated package.json and -lock files. added company.js file.

--- schema.sql
+++ schema.sql
@@ -4,13 +4,13 @@ CREATE database company_tracker;
 USE company_tracker;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT,
     title VARCHAR(30 NOT NULL),
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL 
@@ -18,7 +18,7 @@ CREATE TABLE role (
 )
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
